2329cb147950cf153327f53311d77c5c4e0b5b6f
vamsi-1438 <chirunavu1438@gmail.com>
2022-08-11 11:46:20 +0000

    congratulation card

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="container">
+    <h1 className="heading"> Congratulations</h1>
+    <div className="container2">
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <p className="name">kiran V</p>
+      <p className="paragraph">
+        Vishnu institute of computer Education and Technology Bhimavaram
+      </p>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
